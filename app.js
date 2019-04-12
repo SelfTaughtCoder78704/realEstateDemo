c21a62b65c1914abf0bac899007c46da62447473
Bobby Nicholson <bobbynicholson78704@gmail.com>
2019-04-12 16:17:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 const express = require('express')
-const PORT = 3000
+
+const path = require('path')
+
 const bodyParser = require('body-parser');
 const KEY = require('./config/keys')
+
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const indexRouter = require('./routes/index-routes')
 const dashboardRouter = require('./routes/dashboard')
+
+
 const app = express()
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-
+// Middleware
 //Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+//Method Override
+app.use(methodOverride('_method'))
 
 
+// Static folder
+app.use(express.static('public'))
+app.use('/uploads/',express.static('uploads'))
+// View Engine
+app.set('view engine', 'ejs')
+
+
+
+// Connect to mLab
 mongoose.connect(KEY,{useNewUrlParser: true})
     .then(() => console.log('MongoDb Connected'))
     .catch(err => console.log(err))
 
 
+
+// Routing
 app.use('/', indexRouter)
 app.use('/dashboard', dashboardRouter)
 
-app.listen(process.env.PORT | PORT, () =>{
+
+// Port setup
+
+const PORT = 3000
+app.listen(process.env.PORT || PORT, () =>{
     console.log(`Server started on ${PORT}`)
 })
